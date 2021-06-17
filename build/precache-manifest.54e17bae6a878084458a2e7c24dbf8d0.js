b63020ad9861faa1853aa7b84dfeba2f0494a60f
jamilshafayat <jamilshafayat8@gmail.com>
2021-06-18 02:14:53 +0600

    Git setup for the project

---
+++ build/precache-manifest.54e17bae6a878084458a2e7c24dbf8d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9febeb86d526d1b324d5a51a06d48b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9aea6cd99fdd22787b",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "67c67848110c53b18a53",
+    "url": "/static/css/main.6a15575c.chunk.css"
+  },
+  {
+    "revision": "de9aea6cd99fdd22787b",
+    "url": "/static/js/2.2e898906.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2e898906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c67848110c53b18a53",
+    "url": "/static/js/main.4e9caede.chunk.js"
+  },
+  {
+    "revision": "6d2cd4d0e332372647de",
+    "url": "/static/js/runtime-main.25e51cbb.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
